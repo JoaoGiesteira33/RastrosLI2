c7475d5d6b9bd5d9f914d154009729c8330fd99c
Julio Goncalves <juliobeites@hotmail.com>
2020-03-15 00:23:02 +0000

    Alteracoes Finaise

--- interface.c
+++ interface.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "interface.h"
 #include "logica.h"
 #include "camadadados.h"
 #define BUF_SIZE 1024
-int interpretador(ESTADO *e)
-{
+
+
+int interpretador(ESTADO *e){
+
     char linha[BUF_SIZE];
     char col[2], lin[2];
-    if (fgets(linha, BUF_SIZE, stdin) == NULL)
+
+    if ( fgets(linha, BUF_SIZE, stdin) == NULL)
         return 0;
-    if (strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2)
+
+    if ( strlen (linha) == 3 && sscanf (linha, "%[a-h]%[1-8]", col, lin) == 2)
     {
         COORDENADA coord = {*col - 'a', *lin - '1'};
-        jogar(e, coord);
-        mostrar_tabuleiro(e);
+        jogar (e, coord);
+        mostrar_tabuleiro (e);
     }
     return 1;
 }
 
-void mostrar_tabuleiro(ESTADO estado)
-{
+void mostrar_tabuleiro (ESTADO estado){
+
     int j, i;
     for (j = 0; j <= 7; j++)
     {
@@ -32,7 +37,7 @@ void mostrar_tabuleiro(ESTADO estado)
                 printf("1");
             else
             {
-                switch (estado.tab[j][i])
+                switch (obter_estado_casa (estado, estado.tab [j][i]))
                 {
                     case PRETA:
                         printf("#");
