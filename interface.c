0c502a211701e2bc76329521f52477be7dcbbedc
Julio Goncalves <juliobeites@hotmail.com>
2020-03-17 23:19:24 +0000

    Alguns erros corrigidose

--- interface.c
+++ interface.c
@@ -6,6 +6,33 @@
 #include "camadadados.h"
 #define BUF_SIZE 1024
 
+// Funcao que inicializa o tabuleiro
+void mostrar_tabuleiro (ESTADO estado){
+
+    int j, i;
+    for (j = 0; j <= 7; j++)
+    {
+        for (i = 0; i <= 7; i++)
+        {
+            switch (obter_estado_casa (estado, estado.tab [j][i]))
+            {
+                case PRETA:
+                    printf("#");
+                    break;
+                case VAZIO:
+                    printf(".");
+                    break;
+                case BRANCA:
+                    printf("*");
+                    break;
+            }
+        }
+    }
+    printf("\n Efetue uma Jogada");
+}
+}
+
+
 // Funcao que inicializa e permite jogar
 int interpretador(ESTADO *e){
 
@@ -19,33 +46,8 @@ int interpretador(ESTADO *e){
     {
         COORDENADA coord = {*col - 'a', *lin - '1'};
         jogar (e, coord);
-        mostrar_tabuleiro (e);
+        mostrar_tabuleiro (*e);
     }
     return 1;
 }
 
-// Funcao que inicializa o tabuleiro
-void mostrar_tabuleiro (ESTADO estado){
-
-    int j, i;
-    for (j = 0; j <= 7; j++)
-    {
-        for (i = 0; i <= 7; i++)
-        {
-            switch (obter_estado_casa (estado, estado.tab [j][i]))
-                {
-                    case PRETA:
-                        printf("#");
-                        break;
-                    case VAZIO:
-                        printf(".");
-                        break;
-                    case BRANCA:
-                        printf("*");
-                        break;
-                }
-            }
-        }
-        printf("\n Efetue uma Jogada");
-    }
-}
--- interface.h
+++ interface.h
@@ -1,7 +1,8 @@
 #ifndef RASTROSLI2_INTERFACE_H
 #define RASTROSLI2_INTERFACE_H
 
-#include <camadadados.h>
+#include "camadadados.h"
+
 int interpretador(ESTADO *e);
 void mostrar_tabuleiro(ESTADO estado);
 #endif //RASTROSLI2_INTERFACE_H
