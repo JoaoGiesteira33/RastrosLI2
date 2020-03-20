be21eecf086411a4df03bcb54e955378d2823a7e
DanielXavier114 <danielsxavier7@gmail.com>
2020-03-20 14:48:54 +0000

    documentação

--- camadadados.h
+++ camadadados.h
@@ -1,40 +1,95 @@
+/**
+@file camadadados.h
+Definição do estado e das funções que o manipulam
+*/
+
 #ifndef RASTROSLI2_CAMADADADOS_H
 #define RASTROSLI2_CAMADADADOS_H
-typedef enum
-{
-    VAZIO,
-    BRANCA,
-    PRETA
+/**
+\brief Tipo de dados para a casa
+*/
+typedef enum {
+    UM = '1',
+    DOIS = '2',
+    VAZIO = '.',
+    BRANCA = '*',
+    PRETA = '#'
 } CASA;
-typedef struct
-{
+
+/**
+\brief Tipo de dados para as coordenadas
+*/
+typedef struct {
     int coluna;
     int linha;
 } COORDENADA;
-typedef struct
-{
+
+/**
+\brief Tipo de dados para a jogada
+*/
+typedef struct {
     COORDENADA jogador1;
     COORDENADA jogador2;
 } JOGADA;
+
+/**
+\brief Tipo de dados para as jogadas
+*/
 typedef JOGADA JOGADAS[32];
-typedef struct
-{
+
+/**
+\brief Tipo de dados para o estado
+*/
+
+typedef struct {
+    /** O tabuleiro */
     CASA tab[8][8];
-    COORDENADA ultima_jogada;
+    /** As jogadas */
     JOGADAS jogadas;
+    /** O número das jogadas, usado no prompt */
     int num_jogadas;
+    /** O jogador atual */
     int jogador_atual;
+    /** O nº de comando, usado no prompt */
+    int num_comando;
+    /** A coordenada da última jogada */
+    COORDENADA ultima_jogada;
 } ESTADO;
 
+/**
+\brief Inicializa um estado
+ */
 ESTADO *inicializar_estado();
 
+/**
+\brief Retorna o jogador atual
+@param e Apontador para o estado
+*/
 int obter_jogador_atual(ESTADO *estado);
 
+/**
+\brief Retorna o número de jogadas
+@param e Apontador para o estado
+*/
 int obter_numero_de_jogadas(ESTADO *estado);
 
+/**
+\brief Obtém a ultima jogada efetuada
+@param e Apontador para o estado
+*/
 COORDENADA *obter_ultima_jogada (ESTADO *estado);
 
+/**
+\brief Converte a ultima jogada
+@param c Apontador paraa coordenada
+*/
 char *conversorultimajogada (COORDENADA *c);
 
+
+/**
+\brief Obtém o estado da casa
+@param e Apontador para o estado
+@param c A coordenada
+*/
 CASA obter_estado_casa(ESTADO *e, COORDENADA c);
 #endif //RASTROSLI2_CAMADADADOS_H
--- interface.h
+++ interface.h
@@ -1,8 +1,22 @@
+/**
+@file interface.h
+Definição da interface do jogo
+*/
+
+
 #ifndef RASTROSLI2_INTERFACE_H
 #define RASTROSLI2_INTERFACE_H
 
 #include "camadadados.h"
-
+/**
+\brief Interpretador de comandos
+@param e Apontador para o estado
+*/
 int interpretador(ESTADO *e);
+
+/**
+\brief Função que mostra o tabuleiro
+@param estado O estado
+*/
 void mostrar_tabuleiro(ESTADO estado);
 #endif //RASTROSLI2_INTERFACE_H
