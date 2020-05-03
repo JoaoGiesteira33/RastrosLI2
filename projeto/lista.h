a1d8710ffc048dfcd88946b0c9c6ede8b65884d3
Julio Goncalves <juliobeites@hotmail.com>
2020-05-03 17:05:55 +0100

    XML do Bot

--- projeto/lista.h
+++ projeto/lista.h
@@ -14,46 +14,40 @@ typedef struct nodo {
     /** apontador para o proximo nodo*/
     struct nodo *prox;
 } Nodo, *LISTA;
-// Cria uma lista vazia
 /**
 \brief Função que cria uma lista ligada
 @returns A lista criada
 */
-LISTA criar_lista();
-// Insere um valor na cabeça da lista
+LISTA criar_lista(); // Cria uma lista vazia
 /**
 \brief Função que insere um nodo no ínicio da lista
 @param l Lista ligada
 @param valor Apontador para o valor do nodo a inserir
 @returns A nova lista
 */
-LISTA insere_cabeca(LISTA L, void *valor);
-// Devolve a cabeça da lista
+LISTA insere_cabeca(LISTA L, void *valor); // Insere um valor na cabeça da lista
 /**
 \brief Função que devolve a cabeça da lista
 @param l Lista ligada
 */
-void *devolve_cabeca(LISTA L);
+void *devolve_cabeca(LISTA L); // Devolve a cabeça da lista
 /**
 \brief Função que insere um nodo no ínicio da lista
 @param l Lista ligada
 @param valor Apontador para o valor do nodo a inserir
 @returns A nova lista
 */
-// Devolve a cauda da lista
-LISTA proximo(LISTA L);
+LISTA proximo(LISTA L); // Devolve a cauda da lista
 /**
 \brief Função que remove a cabeça da lista e devolve a cauda
 @param l Lista ligada
 @returns A nova lista
 */
-// Remove a cabeça da lista (libertando o espaço ocupado) e devolve a cauda
-LISTA remove_cabeca(LISTA L);
+LISTA remove_cabeca(LISTA L); // Remove a cabeça da lista (libertando o espaço ocupado) e devolve a cauda
 /**
 \brief Função que verifica se a lista está vazia
 @param l Lista ligada
 @returns Devolve 1 se a lista estiver vazia
 */
-// Devolve verdareiro se a lista é vazia
-int lista_esta_vazia(LISTA L);
+int lista_esta_vazia(LISTA L); // Devolve verdareiro se a lista é vazia
 #endif //RASTROSLI2_LISTA_H
