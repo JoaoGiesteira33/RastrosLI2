56a5dc16c1880021690393485b9b759714f58f39
João Luís Lopes Giesteira <62028180+JoaoGiesteira33@users.noreply.github.com>
2020-05-03 22:42:56 +0100

    Delete functions_8.js

--- bot/html/search/functions_8.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['verificabotfinal',['verificaBotFinal',['../Bot_8h.html#a12aba8023e2e7c4dbe26cc99c85c44fe',1,'Bot.c']]],
-  ['verificacheckmate',['verificaCheckMate',['../Bot_8h.html#a5805cbe64b0343d64d0b3548d869b176',1,'Bot.c']]],
-  ['vizinhos',['vizinhos',['../Bot_8h.html#a4dbaee936d3b7304064647b65fced6eb',1,'Bot.c']]]
-];
