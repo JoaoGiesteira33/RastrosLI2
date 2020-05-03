13a0a4269c59a5e9cc17d80791254e8ed89b7653
Daniel Silva Xavier <61991368+DanielXavier114@users.noreply.github.com>
2020-05-03 22:49:06 +0100

    Delete all_9.js

--- projeto/html/search/all_9.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['mostrar_5ftabuleiro',['mostrar_tabuleiro',['../interface_8h.html#a4525a57d0cd9ed3c9150e19b67e1dad6',1,'interface.c']]],
-  ['movimentos',['movimentos',['../interface_8h.html#acbc064f5e208515bc1b58b731cc17c94',1,'interface.c']]]
-];
