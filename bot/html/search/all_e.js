7849288f3141e3d306c16c6d0de9a3ae2e117ae3
Daniel Silva Xavier <61991368+DanielXavier114@users.noreply.github.com>
2020-05-03 22:40:30 +0100

    Delete all_e.js

--- bot/html/search/all_e.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['valor',['valor',['../structnodo.html#ab63adcdb83ea1fdcf4fa10f3cafc4a6a',1,'nodo']]],
-  ['vencedor',['vencedor',['../structESTADO.html#afa5918b4f2221416965663d44b14e179',1,'ESTADO']]],
-  ['verificabotfinal',['verificaBotFinal',['../Bot_8h.html#a12aba8023e2e7c4dbe26cc99c85c44fe',1,'Bot.c']]],
-  ['verificacheckmate',['verificaCheckMate',['../Bot_8h.html#a5805cbe64b0343d64d0b3548d869b176',1,'Bot.c']]],
-  ['vizinhos',['vizinhos',['../Bot_8h.html#a4dbaee936d3b7304064647b65fced6eb',1,'Bot.c']]]
-];
