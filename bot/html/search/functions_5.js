f37a12e785082d9174b2625c568860232016cf2c
João Luís Lopes Giesteira <62028180+JoaoGiesteira33@users.noreply.github.com>
2020-05-03 22:44:06 +0100

    Delete functions_5.js

--- bot/html/search/functions_5.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['get_5fjogador_5fatual',['get_jogador_atual',['../Bot_8h.html#ab9c95b014d2e217eae7d081088501963',1,'aux.c']]],
-  ['get_5fultima_5fjogada',['get_ultima_jogada',['../Bot_8h.html#ae89c72e4fa31dcc1eb9ba0fb8ea707e1',1,'aux.c']]],
-  ['gravar',['gravar',['../Bot_8h.html#a8fc889059d5c36644fbc1665d750984f',1,'Bot.c']]]
-];
