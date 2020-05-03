a25885fe61077baa8b137f1d04fb3336a4a1ffc1
Daniel Silva Xavier <61991368+DanielXavier114@users.noreply.github.com>
2020-05-03 22:47:12 +0100

    Delete all_2.js

--- projeto/html/search/all_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['dentrotabuleiro',['dentroTabuleiro',['../camadadados_8h.html#ab30857ddb076ebe58da129e3e7ea7b39',1,'camadadados.c']]],
-  ['devolve_5fcabeca',['devolve_cabeca',['../lista_8h.html#abfcb205f3eb670157be0d1221021714b',1,'lista.c']]]
-];
