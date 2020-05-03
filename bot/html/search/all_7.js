446b6c547619a1c570713e16e817d9aec3f398e6
Daniel Silva Xavier <61991368+DanielXavier114@users.noreply.github.com>
2020-05-03 22:38:05 +0100

    Delete all_7.js

--- bot/html/search/all_7.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['inicializar_5festado',['inicializar_estado',['../Bot_8h.html#a7e0c7e26fb685d9ab501e19b05e6954f',1,'Bot.c']]],
-  ['inicializatab',['inicializatab',['../Bot_8h.html#a6d1e4e9073c0dee1e5457adea4acb3e7',1,'Bot.c']]],
-  ['insere_5fcabeca',['insere_cabeca',['../Bot_8h.html#a37ba5fc3cfddb6bc94d4b54b00bc696e',1,'aux.c']]]
-];
