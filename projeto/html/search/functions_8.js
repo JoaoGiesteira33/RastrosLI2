5649fb47b50f6829b2dbb71d013f2aadda7d0a5f
Júlio Beites Gonçalves <62077844+Corona147@users.noreply.github.com>
2020-05-03 22:55:39 +0100

    Delete functions_8.js

--- projeto/html/search/functions_8.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['ler',['ler',['../interface_8h.html#ab752e7aff17a25212d714bd5f8c7fb12',1,'interface.c']]],
-  ['lista_5festa_5fvazia',['lista_esta_vazia',['../lista_8h.html#a4c10928f7acaa4e3de3760cea0dfd9c0',1,'lista.c']]]
-];
